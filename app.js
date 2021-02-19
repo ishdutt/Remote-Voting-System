9c5c481eaaaeddd06cc522f6e4c7c701e5158342
Sumit Nimborkar <sumitnimborkar11@gmail.com>
2021-02-19 11:01:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');	
+var passwordHash = require('password-hash');
+var cookieParser = require('cookie-parser');
+var app = express();
+app.use( bodyParser.json() )
+app.use(cookieParser());
+app.use(morgan('combined'));
+
+
+
+
+app.use("/", express.static("client"));
+
+
+var username;
+var password;
+
+app.post('/login', function(req, res) {
+    
+	console.log(req.body);
+    username = req.body.username;
+    password = req.body.password;
+    var hashedPassword = passwordHash.generate(password);
+    console.log(hashedPassword);
+    
+    if (username == "admin" && password == "password") {
+
+    	res.status(200).send({ message: hashedPassword});
+
+    } else {
+    	res.status(500).send({ message: 'error' });
+    }
+});
+
+app.post('/auth', function(req, res) {
+	var cookie_pass = req.cookies['auth'];
+	if (passwordHash.verify('password', cookie_pass)) {
+		res.status(200).send({ message: hashedPassword});
+	} else {
+		res.status(500).send({ message: 'error' });
+	}
+});
+
+app.get('/',function(req,res){
+	var cookie_pass = req.cookies['auth'];
+	if (passwordHash.verify('password', cookie_pass)) {
+		res.sendFile(path.join(__dirname, 'client', 'app.html'));
+	} else {
+		console.log('ok');
+	}
+});
+
+app.get('/app', function(req, res){
+	var cookie_pass = req.cookies['auth'];
+	var cookie_otp = req.cookies['show'];
+
+	if (passwordHash.verify('password', cookie_pass) && cookie_otp != null) {
+		//res.sendFile(path.join(__dirname, 'ui', 'clist.html'));
+		res.redirect('/info');
+    }
+     if (cookie_otp == null && passwordHash.verify('password', cookie_pass)) {
+		res.sendFile(path.join(__dirname, 'client', 'app.html'));
+	}
+	else {
+		res.redirect('/');
+	}
+	
+});
+
+
+app.get('/info', function(req, res){
+    res.sendFile(path.join(__dirname, 'client', 'clist.html'));
+});
+app.get('/result', function(req, res){
+    res.sendFile(path.join(__dirname, 'client', 'result.html'));
+});
+
+var port = 8080;
+app.listen(8080, function () {
+  console.log(`app is listening on port ${port}!`);
+});
